d944c1cddbb4f331df1f5daf167d3432b07c9e78
Kukjin Lee <75208878+cptkuk91@users.noreply.github.com>
2021-09-15 01:12:32 +0900

    example Source

---
+++ src/web2/ex7-1.js
@@ -0,0 +1,7 @@
+a = 1;
+console.log(a);
+a = 2;
+console.log(a);
+
+// 하지만 상수의 값은 오류가 발생한다.
+// ex) 1 = 2;
---
+++ src/web2/ex7-8.js
@@ -0,0 +1,7 @@
+var name = 'Lorem';
+
+var letter = 'Dear '+ name +' Ipsum is simply dummy text of the printing and typesetting industry. '+ name +' Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum';
+
+console.log(letter);
+
+// 특정 데이터가 반복해서 사용된다면 일괄적으로 변경할 수 있도록 변수를 만드는 것이 좋습니다.
