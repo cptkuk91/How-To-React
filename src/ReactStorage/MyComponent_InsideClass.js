c8855a8acb5558ec879c6301348a856114a0cb8b
이국진 <cptkuk91@gmail.com>
2021-11-13 00:13:53 +0900

    Class Component Study

---
+++ src/ReactStorage/MyComponent_InsideClass.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class MyComponent extends Component {
+    // default 값과 propTypes를 class 안쪽에서 사용할 수 있습니다.
+    static defaultProps = {
+        name: "기본 이름"
+    };
+
+    static PropTypes = {
+        name: PropTypes.string,
+        favoriteNumber: PropTypes.number.isRequired
+    }
+
+  render() {
+    const { name, children, favoriteNumber } = this.props;
+
+    return (
+      <div>
+        안녕하세요. 제 이름은 {name} 입니다. <br />
+        children값은 {children} 입니다.
+        <br />
+        제가 좋아하는 숫자는 {favoriteNumber} 입니다.
+      </div>
+    );
+  }
+}
+
+// MyComponent.defaultProps = {
+//   name: "기본 이름",
+// };
+
+// MyComponent.propTypes = {
+//   name: PropTypes.string,
+//   favoriteNumber: PropTypes.number.isRequired,
+// };
+
+export default MyComponent;
