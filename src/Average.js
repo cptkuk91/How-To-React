2a53ec303cd44aec67ee20e1f2cb825848faad9f
이국진 <cptkuk91@gmail.com>
2021-11-23 16:15:49 +0900

    Study until Page 218

--- src/Average.js
+++ src/Average.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState, useRef, useCallback, useMemo } from 'react'
 
-function Average() {
+const getAverage = (numbers) => {
+    console.log("평균 값 계산 중..");
+    if(numbers.length === 0){
+        return 0;
+    } else {
+        let sum = numbers.reduce((a, b) => a + b);
+        return sum / numbers.length
+    };
+}
+
+const Average = () => {
+    const [list, setList] = useState([]);
+    const [number, setNumber] = useState('');
+    const inputEl = useRef(null);
+
+    const onChange = useCallback((e) => {
+        setNumber(e.target.value);
+    }, []);
+
+    const onInsert = useCallback((e) => {
+        // concat을 통해 list에 넣어주자.
+        let inputList = list.concat(parseInt(number));
+        setList(inputList);
+        setNumber('');
+        inputEl.current.focus();
+    }, [list, number]);
+
+    const avg = useMemo(() => getAverage(list), [list]);
+
     return (
         <div>
-            
+            <input value={number} onChange={onChange} ref={inputEl}/>
+            <button onClick={onInsert}>등록</button>
+            <ul>
+                {list.map((number, index) =>(
+                    <li key={index}>{number}</li>
+                ))}
+            </ul>
+            <div>
+                <b>평균 값: </b>{avg}
+            </div>
         </div>
     )
 }
--- src/Info.js
+++ src/Info.js
@@ -1,38 +1,31 @@
-import React, { useReducer } from 'react'
-
-function reducer(state, action) {
-    return {
-        ...state,
-        [action.name]: action.value
-    };
-}
+import React from "react";
+import useInputs from "./useInputs";
 
 const Info = () => {
-    const [state, dispatch] = useReducer(reducer, {
-        name: '',
-        nickname: '',
-    });
-    const {name, nickname} = state;
-    const onChange = e => {
-        dispatch(e.target);
-    };
+  const [state, onChange] = useInputs({
+    name: "",
+    nickname: "",
+  });
+  const { name, nickname } = state;
 
-    return(
+  return (
+    <div>
+      <div>
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
+      </div>
+      <div>
         <div>
-            <div>
-                <input name="name" value={name} onChange={onChange}/>
-                <input name="nickname" value={nickname} onChange={onChange}/>
-            </div>
-            <div>
-                <div>
-                    <b>이름: </b>{name}
-                </div>
-                <div>
-                    <b>닉네임: </b>{nickname}
-                </div>
-            </div>
+          <b>이름: </b>
+          {name}
         </div>
-    )
-}
+        <div>
+          <b>닉네임: </b>
+          {nickname}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Info
+export default Info;
