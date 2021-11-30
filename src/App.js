224022f76e8d879ee14e490ec26a63b445a83109
이국진 <cptkuk91@gmail.com>
2021-11-30 23:45:35 +0900

    Study Page Until 252

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
-import CSSModule from './CSSModule';
+import StyledComponent from './StyledComponent';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <CSSModule/>
+        <StyledComponent/>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
